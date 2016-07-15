0d2d4b3bc0e0dfdbfaefafd72d3a88ca73594bdb
Tej Chajed <tchajed@mit.edu>
2016-07-15 10:38:50 -0400

    Clean up documentation and variable usage
    Consistently use s for strings, enforced by Implicit Types

--- regex.v
+++ regex.v
@@ -4,9 +4,11 @@
  * to matching and reasoning about regular expressions and
  * languages. The idea is to consider the derivative of a language L
  * (a set of strings) with respect to a character c, defined as {w |
- * cw in L}. In terms of automata, what does the automata match if fed
- * a c first? It happens that the derivative of a regular expression
- * can be defined purely syntatically.
+ * cw in L}. In terms of automata, what does the automata match if fed the
+ * character c first? It turns out that derivatives of regular languages have
+ * two nice properties: the derivative of a regular language is regular, and the
+ * derivative of a language expressed as a regular expression can be expressed
+ * in a simple syntatic manner.
  *
  * This development defines regexes as syntax and as predicates,
  * defines derivatives syntactically and as a transformation of a
@@ -14,6 +16,9 @@
  * two. Specifically, [[ dr/dc ]] = d [[ r ]]/dc, abusing d/dc for
  * both the syntatic and denotational derivative and using [[ r ]] to
  * denote the denotation of a regex.
+ *
+ * These derivative tools give a simple and natural regex matcher with a
+ * correctness proof in its type.
  *)
 
 Require Import List.
@@ -27,13 +32,40 @@ Section RegularExpressions.
   constraint). *)
   Variable Sigma_dec : forall (c c': Sigma), {c = c'} + {c <> c'}.
 
-  Definition string_dec : forall (s s': list Sigma), {s = s'} + {s <> s'} :=
+  (** * Standard names
+
+  These simple definitions give standard names from the theory of computation to
+  the analogues in this setting.
+   *)
+
+  (** A string takes elements from the (arbitrary) alphabet above. *)
+  Definition string := list Sigma.
+  (** A language is traditionally a set of strings. In Coq's constructive logic,
+  the best to express (possibly infinite) sets is as a predicate over strings;
+  the interpretation will be that forall (l: language) (s: string), l s is true
+  for exactly those strings in the language l *)
+  Definition language := string -> Prop.
+
+  (** A cool feature of Coq, mirroring standard practice in programming languages papers.
+
+  This command means, for example, that when we use the letter s for a variable
+  without a type annotation, it should default to being of type string, before
+  types are inferred.
+
+   This might seem like a horrible thing to do, and it is indeed confusing when
+   done on a whiteboard or in a paper, but here it serves an important purpose:
+   it ensures that when we use the letters c, s, or l (or those characters
+   followed by numbers or prime symbols ' actually), they must represent the
+   expected type. *)
+  Implicit Types (c: Sigma) (s: string) (l: language).
+
+  Definition string_dec : forall (s s': string), {s = s'} + {s <> s'} :=
     List.list_eq_dec Sigma_dec.
 
   Inductive regex :=
     (** Empty is the empty language, matching no strings. *)
   | Empty
-  | Char (s:Sigma)
+  | Char (c:Sigma)
   | Or (r1:regex) (r2:regex)
   | Seq (r1:regex) (r2:regex)
   | Star (r:regex).
@@ -41,36 +73,46 @@ Section RegularExpressions.
   (** Eps is a derived regex that matches only the empty string. *)
   Definition Eps := Star Empty.
 
-  (** First we give an auxiliary inductive definition for the
-  denotation of Kleene star of an arbitrary proposition. *)
-  Inductive star (P: list Sigma -> Prop) : list Sigma -> Prop :=
-  | star_empty : star P nil
+  (** First we give an auxiliary inductive definition to take the Kleene star of
+  a language. *)
+  Inductive star (l: language) : language :=
+  | star_empty : star l nil
   | star_iter : forall s1 s2,
-      P s1 ->
-      star P s2 ->
-      star P (s1 ++ s2).
+      l s1 ->
+      star l s2 ->
+      star l (s1 ++ s2).
 
-  (** The denotation of a regex is a language, expressed as a
-  predicate over strings. *)
-  Fixpoint denotation (r:regex) : list Sigma -> Prop :=
+  (** The denotation of a regex is a language.
+
+      Regular expressions give a nice way to think about denotational vs
+      operational definitions. We could define what a regular expression does in
+      terms of an automata, which gives regexes a meaning operationally.
+      Instead, we give a denotation (think "interpretation" or "meaning") for
+      each regex, mapping them to a concept we have in our metatheory.
+
+      Normally the metatheory is mathematics, but here we are using the Coq
+      programming language as the metatheory. In mathematics we would use a set
+      of strings as the denotation of a regex, whereas here we use [language] as
+      defined above. *)
+  Fixpoint denotation (r:regex) : language :=
     match r with
     | Empty => fun _ => False
-    | Char s => fun l => l = s::nil
-    | Or r1 r2 => fun l => denotation r1 l \/ denotation r2 l
-    | Seq r1 r2 => fun l =>
-                    exists l1 l2, l = l1 ++ l2 /\
-                             denotation r1 l1 /\
-                             denotation r2 l2
-    | Star r => fun l => star (denotation r) l
+    | Char c => fun s => s = c::nil
+    | Or r1 r2 => fun s => denotation r1 s \/ denotation r2 s
+    | Seq r1 r2 => fun s =>
+                    exists s1 s2, s = s1 ++ s2 /\
+                             denotation r1 s1 /\
+                             denotation r2 s2
+    | Star r => fun s => star (denotation r) s
     end.
 
   (* We will want to automatically prove the denotation star with its
   constructors *)
   Hint Constructors star.
 
-  Lemma star_false : forall l,
-      star (fun _ => False) l ->
-      l = nil.
+  Lemma star_false : forall s,
+      star (fun _ => False) s ->
+      s = nil.
   Proof.
     inversion 1; intuition.
   Qed.
@@ -96,13 +138,13 @@ Section RegularExpressions.
            | [ H: _ :: _ = _ :: _ |- _ ] =>
              inversion H; clear H
            | _ => progress (intuition eauto 10)
-           | [ |- exists (_ : list _), _ ] =>
+           | [ |- exists (_ : string), _ ] =>
              solve [ exists nil; crush ]
            | _ => congruence
            end.
 
   (* Eps indeed represents the language consisting only of the empty string. *)
-  Remark eps_denotation : forall l, denotation Eps l <-> l = nil.
+  Remark eps_denotation : forall s, denotation Eps s <-> s = nil.
   Proof.
     crush.
   Qed.
@@ -123,12 +165,12 @@ Section RegularExpressions.
   is defined) and nil is in r *)
   Section ObservationMap.
 
-    Lemma observation_map_1 : forall r l,
-      denotation (observation_map r) l ->
-      l = nil.
+    Lemma observation_map_1 : forall r s,
+      denotation (observation_map r) s ->
+      s = nil.
     Proof.
       induction r; crush.
-      rewrite (IHr1 l1); eauto.
+      rewrite (IHr1 s1); eauto.
     Qed.
 
     Lemma observation_map_2 : forall r,
@@ -138,9 +180,9 @@ Section RegularExpressions.
       induction r; crush.
     Qed.
 
-    Lemma observation_map_eps : forall r l,
-        denotation (observation_map r) l ->
-        (l = nil /\ denotation r nil).
+    Lemma observation_map_eps : forall r s,
+        denotation (observation_map r) s ->
+        (s = nil /\ denotation r nil).
     Proof.
       intros.
       pose proof (observation_map_1 _ _ H); subst.
@@ -160,6 +202,8 @@ Section RegularExpressions.
   (** Finally, we define the syntactic continuation map or derivative
     of a regular expression, with respect to c. *)
   Section Derivative.
+    (** This is the character we are taking the derivative with respect to.
+    Making it a variable means we don't have to pass it in recursive calls. *)
     Variable c:Sigma.
 
     Fixpoint continuation_map (r:regex) : regex :=
@@ -177,34 +221,35 @@ Section RegularExpressions.
   (** To write the correctness of the continuation_map, [derivative]
   defines derivative for a language in a straightforward,
   interpretable way. *)
-  Definition derivative (c:Sigma) (P: list Sigma -> Prop) : list Sigma -> Prop :=
-    fun l => P (c :: l).
+  Definition derivative (c:Sigma) (l: language) : language :=
+    fun s => l (c :: s).
 
   Hint Resolve app_comm_cons.
   Hint Resolve observation_map_eps observation_map_holds.
 
   Theorem continuation_map_denotes_derivative_1 : forall r c,
-      forall l, denotation (continuation_map c r) l ->
-           derivative c (denotation r) l.
+      forall s, denotation (continuation_map c r) s ->
+           derivative c (denotation r) s.
   Proof.
     unfold derivative.
     induction r; crush.
-    - destruct (Sigma_dec c s); crush.
+    - destruct (Sigma_dec c0 c); crush.
+    - do 2 eexists; crush.
     - pose proof (observation_map_eps _ _ H); crush.
     - rewrite app_comm_cons.
       eauto.
   Qed.
 
   Theorem continuation_map_denotes_derivative_2 : forall r c,
-      forall l, derivative c (denotation r) l ->
-           denotation (continuation_map c r) l.
+      forall s, derivative c (denotation r) s ->
+           denotation (continuation_map c r) s.
   Proof.
     unfold derivative.
     induction r; crush.
-    - destruct (Sigma_dec s s); crush.
-    - destruct l1; solve [ left + right; crush ].
-    - remember (c::l).
-      generalize dependent l.
+    - destruct (Sigma_dec c c); crush.
+    - destruct s1; solve [ left + right; crush ].
+    - remember (c::s).
+      generalize dependent s.
       match goal with
       | [ H: star _ _ |- _ ] => induction H; crush
       end.
@@ -212,8 +257,8 @@ Section RegularExpressions.
   Qed.
 
   Theorem continuation_map_denotes_derivative : forall r c,
-      forall l, denotation (continuation_map c r) l <->
-           derivative c (denotation r) l.
+      forall s, denotation (continuation_map c r) s <->
+           derivative c (denotation r) s.
   Proof.
     split; auto using continuation_map_denotes_derivative_1,
            continuation_map_denotes_derivative_2.
@@ -224,7 +269,9 @@ Section RegularExpressions.
     Ltac t := try solve [ left + right; crush ].
 
     (** auxilliary function to make observation_map computable *)
-    Definition includes_nil r : {denotation (observation_map r) nil} + {forall l, ~denotation (observation_map r) l}.
+    Definition includes_nil r :
+      {denotation (observation_map r) nil} +
+      {forall s, ~denotation (observation_map r) s}.
       induction r; crush; t.
     Defined.
 
@@ -272,3 +319,7 @@ Section RegularExpressions.
   End Matching.
 
 End RegularExpressions.
+
+(* Local Variables: *)
+(* company-coq-local-symbols: (("Sigma" . ?Σ)) *)
+(* End: *)
